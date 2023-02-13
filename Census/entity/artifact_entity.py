ca2761ba53e2eeb7b99dd1c5932cad4eda9343d0
vivek mishra <starvivek1996@gmail.com>
2023-02-13 15:40:04 +0530

    model_trainer completed

--- Census/entity/artifact_entity.py
+++ Census/entity/artifact_entity.py
@@ -18,3 +18,10 @@ class DataTransformationArtifact:
     transformed_test_path:str
     target_encoder_path:str
     input_feature_encoder_path:str
+
+
+@dataclass
+class ModelTrainerArtifact:
+    model_path:str 
+    f1_train_score:float 
+    f1_test_score:float
--- Census/pipeline/training_pipeline.py
+++ Census/pipeline/training_pipeline.py
@@ -6,6 +6,7 @@ from Census.entity import config_entity
 from Census.components.data_ingestion import DataIngestion
 from Census.components.data_validation import DataValidation
 from Census.components.data_transformation import DataTransformation
+from Census.components.model_trainer import ModelTrainer
 
 
 def start_training_pipeline():
@@ -30,6 +31,11 @@ def start_training_pipeline():
         data_transformation = DataTransformation(data_transformation_config=data_transformation_config, 
         data_ingestion_artifact=data_ingestion_artifact)
         data_transformation_artifact = data_transformation.initiate_data_transformation()
+
+        #model trainer
+        model_trainer_config = config_entity.ModelTrainerConfig(training_pipeline_config=training_pipeline_config)
+        model_trainer = ModelTrainer(model_trainer_config=model_trainer_config, data_transformation_artifact=data_transformation_artifact)
+        model_trainer_artifact = model_trainer.initiate_model_trainer()
         
         
     except Exception as e:
